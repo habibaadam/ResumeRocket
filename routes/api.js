75bd5e947a59e69ee40363bc2e86ddbedf135ea3
OteIan <oteian9@gmail.com>
2024-05-22 21:04:30 +0300

    New: Logout endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { createNew, forLogin } = require('../controllers/userController');
+const { createNew, forLogin, forLogout } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', createNew); // handler for signingUp
 router.post('/login', forLogin); // handler for logging in
+router.post('/logout', forLogout); // handler for logging out
 
 module.exports = router;
