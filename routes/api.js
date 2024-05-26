76a378430c9db4cef559d0c96d5788711ff003e8
OteIan <oteian9@gmail.com>
2024-05-26 17:00:16 +0300

    Enpoints for fetching the cv and recommendation

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 
 const { createNewUser, forLogin, forLogout } = require('../controllers/userController');
-const { createNewCV } = require('../controllers/cvController');
-const { createNewRec } = require('../controllers/recController');
+const { createNewCv, getCv } = require('../controllers/cvController');
+const { createNewRec, getRec } = require('../controllers/recController');
 
 const router = express.Router();
 
@@ -11,8 +11,10 @@ router.post('/signup', createNewUser); // handler for signingUp
 router.post('/login', forLogin); // handler for logging in
 router.post('/logout', forLogout); // handler for logging out
 
-router.post('/cv', createNewCV); // handler for creating a new cv
+router.post('/newCV', createNewCv); // handler for creating a new cv
+router.get('/getCV/:id', getCv); // handler for getting a cv
 
-router.post('/rec', createNewRec); // handler for creating a new rec
+router.post('/newRec', createNewRec); // handler for creating a new rec
+router.get('/getRec/:id', getRec); // handler for getting a rec
 
 module.exports = router;
