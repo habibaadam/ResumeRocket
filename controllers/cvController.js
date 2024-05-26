37709da1330a4bd55c04c7aa8a8bc99af9dd3a76
OteIan <oteian9@gmail.com>
2024-05-26 17:01:18 +0300

    Updated endpoints to fetch the cv and Rec

--- controllers/cvController.js
+++ controllers/cvController.js
@@ -1,4 +1,5 @@
 const CV = require('../models/cv');
+const User = require('../models/User');
 
 exports.createNewCv = async function createNewCv(req, res) {
     const { user, content } = req.body;
@@ -14,14 +15,30 @@ exports.createNewCv = async function createNewCv(req, res) {
             user,
             content,
         });
+        const existingUser = await User.findOne({ user });
+
         const savedCv = await newCv.save();
         return res.status(200).json({
             message: 'Sucessfully created cv!',
-            user: savedCv.user,
+            user: `${existingUser.firstName} ${existingUser.lastName}`,
             content: savedCv.content,
         })
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal Server Error' });
     }
+}
+
+exports.getCv = async function getCv(req, res) {
+    const cv = await CV.findOne({ _id: req.params.id });
+    if (!cv) {
+        return res.status(404).json({ message: 'No cv found' });
+    }
+    const user = await User.findOne({ _id: cv.user });
+
+    return res.status(200).json({
+        message: 'Sucessfully fetched cv!',
+        user: `${user.firstName} ${user.lastName}`,
+        content: cv.content,
+    })
 }
