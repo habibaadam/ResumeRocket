e940931ae4bc228b36cbc089d2cf0303da88b7b0
MarkMusili <musiliyrn@gmail.com>
2024-05-26 14:22:39 +0300

    AI endpoint added

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
+const {
+  createNew, forLogin, forLogout, getUser,
+} = require('../controllers/userController');
+const { ai } = require('../controllers/aiController');
 
-const { createNew, forLogin, forLogout, getUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', createNew); // handler for signingUp
 router.post('/login', forLogin); // handler for logging in
 router.post('/logout', forLogout); // handler for logging out
-router.get('/user/:id', getUser); // handler for getting a user
+router.get('/user/:id', getUser); // handler for getting a user8
+router.post('/ai', ai); // handler for querying the ai model
 
 module.exports = router;
