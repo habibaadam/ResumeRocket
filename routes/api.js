2e396b93b2a37973cbb7f5ac45cf93de0dc4658d
OteIan <oteian9@gmail.com>
2024-05-26 15:59:05 +0300

    Added new endpoints for cv and recommendations

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 
-const { createNew, forLogin, forLogout } = require('../controllers/userController');
+const { createNewUser, forLogin, forLogout } = require('../controllers/userController');
+const { createNewCV } = require('../controllers/cvController');
+const { createNewRec } = require('../controllers/recController');
+
 const router = express.Router();
 
-router.post('/signup', createNew); // handler for signingUp
+router.post('/signup', createNewUser); // handler for signingUp
 router.post('/login', forLogin); // handler for logging in
 router.post('/logout', forLogout); // handler for logging out
+
+router.post('/cv', createNewCV); // handler for creating a new cv
+
+router.post('/rec', createNewRec); // handler for creating a new rec
 
 module.exports = router;
