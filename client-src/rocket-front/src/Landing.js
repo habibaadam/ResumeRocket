c9be1edc23986ded375bf98539d73d98d6e85f9d
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-05-27 10:03:08 +0000

    updates

--- client-src/rocket-front/src/Landing.js
+++ client-src/rocket-front/src/Landing.js
@@ -20,14 +20,17 @@ export default function Landing() {
     </button>
 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link resume" href="../">ResumeRocket</a>
-      </div>
+        <a className="nav-link " href="../">ResumeRocket</a>
+        <a className="nav-link left" href="../">SignUp</a>
+        <a className="nav-link left-2" href="../">Log In</a>
+        <a className="nav-link left-3" href="../">Contact</a>
+        </div>
 </div>
   </div>
 </nav>
           <h1 className="text-center">Welcome To <span className="resume">ResumeRocket</span></h1>
           <p className="text-center">An AI-powered resume builder specifically designed for individuals</p>
-          <small>seeking to venture into or are in the tech industry, particularly software engineering</small>
+          <small className="text-center">seeking to venture into or are in the tech industry, particularly software engineering</small>
           <button type="button" className="mt-5 btn btn-secondary">Get Started</button>
       </div>
       );
---
+++ client-src/rocket-front/src/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function SignUp() {
+  return (
+    // sign up form page
+    <div />
+  );
+}
