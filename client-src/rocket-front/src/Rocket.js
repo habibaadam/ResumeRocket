44d08c12b48fd3070a3d5e2f1aefe62d62dd45a6
OteIan <oteian9@gmail.com>
2024-06-01 14:44:30 +0300

    Indentation

--- client-src/rocket-front/src/Rocket.js
+++ client-src/rocket-front/src/Rocket.js
@@ -26,12 +26,12 @@ export default function Rocket() {
   ];
 
   const handleAnswer = async () => {
-  const textarea = document.getElementById('answer');
+    const textarea = document.getElementById('answer');
     const currentA = textarea.value + '.';
     // append current answer to the state variable
     setAnswer(previousA => [...previousA, currentA])
 
-     // clear text area after user is done answering
+    // clear text area after user is done answering
     textarea.value = '';
 
     // when user clicks on button check, display next question
@@ -41,7 +41,7 @@ export default function Rocket() {
       const joinedA = answer.join(' '); // joins answers together
       console.log(joinedA);
     }
-}
+  }
 
 
   return (
@@ -58,32 +58,32 @@ export default function Rocket() {
         </div>
 
         <div className="full-height right col-md-9">
-                <nav className="navbar navbar-expand-lg mt-2" data-bs-theme="dark">
-        <div className="container-fluid">
-        <a className="navbar-brand" href="ahjsgjhdg">
-      <img
-        src={logo}
-        height="40"
-        alt="ResumeRocket Logo"
-        loading="lazy"
-      />
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="Namenavbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <Link className="nav-link resume " to="/">ResumeRocket</Link>
-        <div className="left-side-links resume">
-        <Link className="nav-link" to="/">About Us</Link>
-        <Link className="nav-link" to="../">Contact</Link>
-        </div>
-      </div>
-</div>
-  </div>
-</nav>
-      <h1 className="text-center mt-4">Hello, <span className="resume">{firstName}</span></h1>
-      <p className="text-center">Kindly answer these questions as accurate as you can in sentences! </p>
+          <nav className="navbar navbar-expand-lg mt-2" data-bs-theme="dark">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="ahjsgjhdg">
+                <img
+                  src={logo}
+                  height="40"
+                  alt="ResumeRocket Logo"
+                  loading="lazy"
+                />
+              </a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="Namenavbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <Link className="nav-link resume " to="/">ResumeRocket</Link>
+                  <div className="left-side-links resume">
+                    <Link className="nav-link" to="/">About Us</Link>
+                    <Link className="nav-link" to="../">Contact</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+          <h1 className="text-center mt-4">Hello, <span className="resume">{firstName}</span></h1>
+          <p className="text-center">Kindly answer these questions as accurate as you can in sentences! </p>
 
           <div className="sexy-box bg-dark">
             <div className="d-flex">
@@ -96,13 +96,13 @@ export default function Rocket() {
               </h2>
             </div>
 
-<div className="input-group mt-5">
-  <textarea rows="2" className="form-control" placeholder="Type your answer here..." aria-label="Type your answer here..." aria-describedby="button-addon2" id="answer"/>
-  <button className="btn btn-outline-secondary"  onClick={handleAnswer} type="button" id="button-addon2">
-    <FontAwesomeIcon icon={faCheck} />
-  </button>
-</div>
-</div>
+            <div className="input-group mt-5">
+              <textarea rows="2" className="form-control" placeholder="Type your answer here..." aria-label="Type your answer here..." aria-describedby="button-addon2" id="answer" />
+              <button className="btn btn-outline-secondary" onClick={handleAnswer} type="button" id="button-addon2">
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+            </div>
+          </div>
 
         </div>
       </div>
