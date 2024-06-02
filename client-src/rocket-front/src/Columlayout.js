5c807520a9104fb9011bcfc5efc8cba4db6e7a3a
memoe <memoemukoe@gmail.com>
2024-06-02 13:47:32 +0200

    50% working on the scolling page left wih intergrating the coreect images

--- client-src/rocket-front/src/Columlayout.js
+++ client-src/rocket-front/src/Columlayout.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import './manual.css';
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
 
 
 export default function Columlayout() {
   return (
     <div className="contain">
+      <h1 className="guide">Unleash Your Journey: A Visual Guide to Master Our Page</h1>
       <div className="row h-100">
-        <div className="col-md-3 h-100 bg-red-600">
-          test
-          test
-          test
-          test
+        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
+          <img className="image_classs" src={image1} alt="image1" />
+          <p className="paragraph">Simply click on get started</p>
         </div>
-        <div className="col-md-3 h-100 bg-white">
-          test
-          test
-          test
-          test
+        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
+          <img className="image_classs" src={image2} alt="image2" />
+          <p className="paragraph">Follow the prompts to register</p>
         </div>
-        <div className="col-md-3 h-100 bg-slate-600" >
-          test
-          test
-          test
-          test
+        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
+          <img className="image_classs" src={image3} alt="image3" />
+          <p className="paragraph"> create your Resume</p>
         </div>
-        <div className="col-md-3 h-100 bg-yellow-400">
-          test
-          test
-          test
-          test
+        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
+          <img className="image_classs" src={image1} alt="image4" />
+          <p className="paragraph">Download your resume🤩</p>
         </div>
       </div>
     </div>
