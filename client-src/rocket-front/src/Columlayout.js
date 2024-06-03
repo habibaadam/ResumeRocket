fec1b8bb0dbc48aedcbcb0e2ac4b9aa1148ca6c4
memoe <memoemukoe@gmail.com>
2024-06-03 17:55:15 +0200

    the scrolling page and pictures done

--- client-src/rocket-front/src/Columlayout.js
+++ client-src/rocket-front/src/Columlayout.js
@@ -2,30 +2,26 @@ import React from 'react';
 import './manual.css';
 import image1 from './images/image1.jpg';
 import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
+import image4 from './images/image4.jpg';
 
 
 export default function Columlayout() {
   return (
     <div className="contain">
-      <h1 className="guide">Unleash Your Journey: A Visual Guide to Master Our Page</h1>
       <div className="row h-100">
-        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
+        <h1 className="guide">Unleash Your Journey: A Visual Guide to Master Our Page</h1>
+        <div className="col-md-4 h-100 bg-red-600 d-flex flex-column">
+          <p>SIMPLY CLICK ON GET <br></br> STARTED</p>
           <img className="image_classs" src={image1} alt="image1" />
-          <p className="paragraph">Simply click on get started</p>
-        </div>
-        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
+          </div>
+        <div className="col-md-4 h-100  bg-slate-600 d-flex flex-column">
+          <p className="paragraph">REGISTER YOUR ACCOUNT<br></br> AND SET PASSWORD</p>
           <img className="image_classs" src={image2} alt="image2" />
-          <p className="paragraph">Follow the prompts to register</p>
-        </div>
-        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
-          <img className="image_classs" src={image3} alt="image3" />
-          <p className="paragraph"> create your Resume</p>
-        </div>
-        <div className="col-md-3 h-100 bg-red-600 d-flex flex-column">
-          <img className="image_classs" src={image1} alt="image4" />
-          <p className="paragraph">Download your resume🤩</p>
-        </div>
+          </div>
+        <div className="col-md-4 h-100 bg-yellow-400 d-flex flex-column">
+          <p className="paragraph"> ANSWER QUESTIONS  AND <br></br> GENERATE YOUR RESUME</p>
+          <img className="image_classs" src={image4} alt="image4" />
+          </div>
       </div>
     </div>
   );
