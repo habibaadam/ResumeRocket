560b991ebb87e290089220df3f4921b057303f8a
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-06-04 19:00:46 +0000

    Formatting ai response and Pdf styling

--- client-src/rocket-front/src/allComponents/pdfGenerator.js
+++ client-src/rocket-front/src/allComponents/pdfGenerator.js
@@ -1,6 +1,10 @@
 import axios from "axios";
-import React from "react";
+import React  from "react";
 import '../allStyles/pdfGenerator.css';
+import { pdf } from '@react-pdf/renderer';
+import PdfDocument from "./pdfStyle";
+import fileDownload from 'js-file-download';
+
 
 export default function PdfGenerator({ userPrompt }) {
   const handleGeneratePdf = async () => {
@@ -12,30 +16,24 @@ export default function PdfGenerator({ userPrompt }) {
         throw new Error("User not found in localStorage");
       }
 
-      const aiResponse = await axios.post("http://localhost:5000/generateCV", {
+      const response = await axios.post("http://localhost:5000/generateCV", {
         user: userId,
         prompt: userPrompt,
       });
+      console.log(response);
+
+      const content = JSON.parse(response.data.content);
+      const aiResponse = content.text;
       console.log('aiResponse:', aiResponse);
 
-      const response = await axios.post("http://localhost:5000/generatePDF", {
-        content: aiResponse.data.content,
-      }, {
-        responseType: 'blob',
-      });
-      console.log('response.data:', response.data);
-      const pdfBlob = new Blob([response.data], { type: 'application/pdf' });
-      const downloadUrl = URL.createObjectURL(pdfBlob);
+      //const forPdf = marked(aiResponse);
 
-      const link = document.createElement('a');
-      link.href = downloadUrl;
-      link.download = 'CV.pdf'; // name of the downloaded file
-      link.click();
-
+      const blob = await pdf(<PdfDocument content={aiResponse} />).toBlob();
+      fileDownload(blob, 'yourcv.pdf');
     } catch (error) {
       console.error(error);
     }
-  }
+}
   return (
     <div className="final-button">
       <button className="btn btn-secondary fn mt-5" onClick={handleGeneratePdf}>Generate CV</button>
