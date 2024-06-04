9ed52be67356576fc86334fd4cf5692d298ca6c7
MarkMusili <musiliyrn@gmail.com>
2024-06-04 20:18:28 +0300

    New AI model from OPENAI

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -1,33 +1,26 @@
-const fetch = require('node-fetch');
-
-global.fetch = fetch;
-global.Headers = fetch.Headers;
-const { GoogleGenerativeAI } = require('@google/generative-ai');
+const OpenAI = require('openai');
 const dotenv = require('dotenv');
 
-exports.ai = async (req, res) => {
-  try {
-    dotenv.config();
-    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-    const generationConfig = {
-      temperature: 0.5,
-      top_p: 1,
-      top_k: 0,
-      max_output_tokens: 10000,
-    };
+dotenv.config();
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
-    //Temporarily using the gemini model on its own
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro', generationConfig });
-    const { prompt } = req.body;
-    const headers = {
-      'Content-Type': 'application/json',
+exports.ai  = async (req, res) => {
+    try {
+    const prompt = req.body.prompt;
+    const model = process.env.OPENAI_MODEL;
+    const params = {
+        messages: [
+                { role: 'system', content: "Given your personal details, seniority, role applying for, education, experience, technologies you're familiar with, familiarity with Git, databases, cloud, and specialized technologies, projects, certifications, and soft skills, generate a comprehensive and well-formatted CV for a software engineering role. Include a summary that explains why you're applying for the job and your preferred working environment(under the summary)."},
+                { role: 'user', content: prompt },
+        ],
+        model: model,
     };
-    const result = await model.generateContent(prompt, { headers });
-    const response = await result.response;
-    const text = response.text();
-    // Here you can do whatever you want with the text
-    res.status(200).json({ text });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+    const chatCompletion = await openai.chat.completions.create(params);
+    res.json(chatCompletion.choices[0].message.content);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
