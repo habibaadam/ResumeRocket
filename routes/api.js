5a584bf7ab84a14de61bff2436028a5ab97d3f3e
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-06-05 16:12:17 +0000

    Deleted: endpoint for generating pdf in the backend

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { ai } = require('../controllers/aiController');
 
 const { createNew, forLogin, forLogout, getUser } = require('../controllers/userController');
-const { createNewCv, getCv, generatePDF, generateCv } = require('../controllers/cvController');
+const { createNewCv, getCv, generateCv } = require('../controllers/cvController');
 const { createNewRec, getRec } = require('../controllers/recController');
 
 const router = express.Router();
@@ -15,7 +15,6 @@ router.get('/user/:id', getUser); // handler for getting a user
 
 router.post('/newCV', createNewCv); // handler for creating a new cv
 router.get('/getCV/:id', getCv); // handler for getting a cv
-router.post('/generatePDF', generatePDF); // handler for generating a pdf
 router.post('/generateCV', generateCv); // handler for generating a cv
 
 router.post('/newRec', createNewRec); // handler for creating a new rec
