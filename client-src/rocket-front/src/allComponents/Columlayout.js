6a8ced9c6a783a8610944778a6ba204fae744fee
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-07-02 15:59:44 +0000

    Column layout responsive changes

--- client-src/rocket-front/src/allComponents/Columlayout.js
+++ client-src/rocket-front/src/allComponents/Columlayout.js
@@ -12,7 +12,7 @@ export default function Columlayout() {
   return (
     <div className="contain">
       <div className="row h-100">
-        <h1 className="mt-5 text-center resume">How It Works</h1>
+        <h1 className="mt-5 mb-5 text-center resume">How It Works</h1>
         <div
           className="col-md-3 d-flex" // Changed from col-md-4 to col-md-3
           data-aos="fade-up"
@@ -21,13 +21,12 @@ export default function Columlayout() {
           <div className="containers">
             <div className="icons">
               <FontAwesomeIcon icon={faPlay} />
-              <h6 className="para">Click GetStarted </h6>
+              <h6 className="para">To Get Started </h6>
+
+              <hr />
               <div className="info">
                 <ul>
-                <li>Click the GetStarted Button </li>
-                <li> lt will lead you to registering page</li>
-                <li>You will provide email details </li>
-                  <li>And create a password </li>
+                <li>Click on the GetStarted button which will lead you to a registering form for an easy and seamless sign up.</li>
                 </ul>
               </div>
             </div>
@@ -41,13 +40,12 @@ export default function Columlayout() {
           <div className="containers">
             <div className="icons">
               <FontAwesomeIcon icon={faUserPlus} />
-              <h6 className="para">Register Account </h6>
+              <h6 className="para">Register Account. </h6>
+              <hr />
               <div className="info">
                 <ul>
-                  <li>After providing account details</li>
-                  <li>Click on the register button</li>
-                  <li>A succesfull registering prompt</li>
-                  <li>Will pop & direct to next page</li>
+                  <li>Provide simple account details</li>
+                  <li>Tap on the register button with a secure, unforgettable password. </li>
                 </ul>
               </div>
             </div>
@@ -61,12 +59,12 @@ export default function Columlayout() {
           <div className="containers">
             <div className="icons">
               <FontAwesomeIcon icon={faQuestionCircle} />
-              <h6 className="para">Answer Questions</h6>
+              <h6 className="para">Answer Q/A In Sentences.</h6>
+              <hr />
               <div className="info">
                 <ul>
-                  <li>The AI page will display your Name</li>
-                  <li>And you will be asked various Questions </li>
-                  <li>Including official Name & Qualifications</li>
+                  <li>Questions relevant to resume generations are asked by our AI</li>
+                  <li>Make sure to answer precisely in a conversational manner. </li>
                 </ul>
               </div>
             </div>
@@ -80,13 +78,12 @@ export default function Columlayout() {
           <div className="containers">
             <div className="icons">
               <FontAwesomeIcon icon={faFileAlt} />
-              <h6 className="para">Generate & Download </h6>
+              <h6 className="para">Generate & Download Cv. </h6>
+
+              <hr />
               <div className="info">
                 <ul>
-                  <li>After answering all questions to your liking</li>
-                  <li>The AI will generate a Resume</li>
-                  <li>And you will be able to</li>
-                  <li>Download  & save your Resume</li>
+                  <li>At the end of all questions, tap on the generate cv button and wait 2-3 mins max to have your cv downloaded automatically!</li>
                 </ul>
               </div>
             </div>
--- client-src/rocket-front/src/allComponents/Wave.js
+++ client-src/rocket-front/src/allComponents/Wave.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function Wave() {
+
   return (
        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900" preserveAspectRatio="xMidYMax slice">
     <rect width="100%" height="100%" fill="#12041F"></rect>
