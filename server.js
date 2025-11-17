54d59df89351fea8ed235c2fac2720f05d6e6321
habibaadam <habiepalmer@gmail.com>
2025-11-17 12:44:06 +0000

    port update

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const api = require('./routes/api.js');
-const datab = require('./utils/db.js');
-const cors = require('cors'); // cross origin sharing for front end
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const api = require('./routes/api.js')
+const datab = require('./utils/db.js')
+const cors = require('cors') // cross origin sharing for front end
 
-const port = process.env.PORT || 5000;
-const app = express();
+const port = process.env.PORT || 3000
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.use('/', api);
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+app.use('/', api)
 
 app.listen(port, () => {
-  console.log(`Server connected and running on port ${port}`)
-});
+    console.log(`Server connected and running on port ${port}`)
+})
